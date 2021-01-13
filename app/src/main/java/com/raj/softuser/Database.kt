394695ef8ab002eafbef3b9ea1a197f36e8b0c2c
rajshrestha416 <rajshrestha416@gmail.com>
2021-01-13 06:13:06 +0545

    Model and Database Added

---
+++ app/src/main/java/com/raj/softuser/Database.kt
@@ -0,0 +1,16 @@
+package com.raj.softuser
+
+import com.raj.softuser.Model.Student
+
+private var studentList = arrayListOf<Student>()
+class Database {
+    fun appendStudent(student: Student){
+        studentList.add(student)
+    }
+    fun returnStudent(): ArrayList<Student>{
+        return studentList
+    }
+    fun deleteStudent(student: Student){
+        studentList.add(student)
+    }
+}
