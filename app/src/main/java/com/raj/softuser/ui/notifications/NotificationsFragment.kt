d74c46a1e7e11ef6d11f9d642729743e3d5ff9df
rajshrestha416 <rajshrestha416@gmail.com>
2021-01-13 11:02:26 +0545

    Web view showed in about us

--- app/src/main/java/com/raj/softuser/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/raj/softuser/ui/notifications/NotificationsFragment.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.raj.softuser.R
-import com.raj.softuser.databinding.FragmentHomeBinding
 import com.raj.softuser.databinding.FragmentNotificationsBinding
+
 
 class NotificationsFragment : Fragment() {
 
@@ -18,13 +17,12 @@ class NotificationsFragment : Fragment() {
     private lateinit var binding: FragmentNotificationsBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        binding.wvSoftwarica.loadUrl("https://softwarica.edu.np/")
-
+        binding.wvSoftwarica.loadUrl("https://softwarica.edu.np/about-softwarica/")
         return binding.root
     }
 }
